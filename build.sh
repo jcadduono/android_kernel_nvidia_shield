73c8c4c7ab3591d242f3bb8bd807ed9d856daecc
James Christopher Adduono <jc@adduono.com>
2016-10-13 18:37:58 -0400

    Updated build scripts

--- build.sh
+++ build.sh
@@ -11,57 +11,54 @@ VER=$(cat "$RDIR/VERSION")
 # directory containing cross-compile arm toolchain
 TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabihf
 
+CPU_THREADS=$(grep -c "processor" /proc/cpuinfo)
 # amount of cpu threads to use in kernel make process
-THREADS=5
+THREADS=$((CPU_THREADS + 1))
 
 ############## SCARY NO-TOUCHY STUFF ###############
+
+ABORT()
+{
+	[ "$1" ] && echo "Error: $*"
+	exit 1
+}
 
 export ARCH=arm
 export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
 
-cd "$RDIR"
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
 
 [ "$TARGET" ] || TARGET=nethunter
-[ "$1" ] && {
-	DEVICE=$1
-} || {
-	DEVICE=shieldtablet
-}
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=shieldtablet
+
 DEFCONFIG=${TARGET}_${DEVICE}_defconfig
 
-[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] || {
-	echo "Config $DEFCONFIG not found in $ARCH configs!"
-	exit 1
-}
+[ -f "$RDIR/arch/$ARCH/configs/${DEFCONFIG}" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
 
-export LOCALVERSION="$TARGET-$DEVICE-$VER"
-
-ABORT()
-{
-	echo "Error: $*"
-	exit 1
-}
+export LOCALVERSION=$TARGET-$DEVICE-$VER
 
 CLEAN_BUILD()
 {
 	echo "Cleaning build..."
-	cd "$RDIR"
 	rm -rf build
 }
 
 SETUP_BUILD()
 {
-	echo "Creating kernel config..."
-	cd "$RDIR"
+	echo "Creating kernel config for $LOCALVERSION..."
 	mkdir -p build
-	make -C "$RDIR" O=build "$DEFCONFIG" || ABORT "Failed to set up build"
+	make -C "$RDIR" O=build "$DEFCONFIG" \
+		|| ABORT "Failed to set up build"
 }
 
 BUILD_KERNEL()
 {
 	echo "Starting build for $LOCALVERSION..."
 	while ! make -C "$RDIR" O=build -j"$THREADS"; do
-		read -p "Build failed. Retry? " do_retry
+		read -rp "Build failed. Retry? " do_retry
 		case $do_retry in
 			Y|y) continue ;;
 			*) return 1 ;;
@@ -69,4 +66,19 @@ BUILD_KERNEL()
 	done
 }
 
-CLEAN_BUILD && SETUP_BUILD && BUILD_KERNEL && echo "Finished building $LOCALVERSION!"
+INSTALL_MODULES() {
+	echo "Installing kernel modules to build/lib/modules..."
+	make -C "$RDIR" O=build \
+		INSTALL_MOD_PATH="." \
+		INSTALL_MOD_STRIP=1 \
+		modules_install
+	rm build/lib/modules/*/build build/lib/modules/*/source
+}
+
+cd "$RDIR" || ABORT "Failed to enter $RDIR!"
+
+CLEAN_BUILD &&
+SETUP_BUILD &&
+BUILD_KERNEL &&
+INSTALL_MODULES &&
+echo "Finished building $LOCALVERSION!"
--- menuconfig.sh
+++ menuconfig.sh
@@ -12,37 +12,43 @@ TOOLCHAIN=$HOME/build/toolchain/gcc-linaro-4.9-2016.02-x86_64_arm-linux-gnueabih
 export ARCH=arm
 export CROSS_COMPILE=$TOOLCHAIN/bin/arm-linux-gnueabihf-
 
-[ "$TARGET" ] || TARGET=nethunter
-[ "$1" ] && {
-	DEVICE=$1
-} || {
-	DEVICE=shieldtablet
-}
-DEFCONFIG=${TARGET}_${DEVICE}_defconfig
-DEFCONFIG_FILE=$RDIR/arch/$ARCH/configs/$DEFCONFIG
-
-[ -f "$DEFCONFIG_FILE" ] || {
-	echo "Config $DEFCONFIG not found in $ARCH configs!"
+ABORT()
+{
+	[ "$1" ] && echo "Error: $*"
 	exit 1
 }
 
-cd "$RDIR"
+[ -x "${CROSS_COMPILE}gcc" ] ||
+ABORT "Unable to find gcc cross-compiler at location: ${CROSS_COMPILE}gcc"
+
+[ "$TARGET" ] || TARGET=nethunter
+[ "$1" ] && DEVICE=$1
+[ "$DEVICE" ] || DEVICE=shieldtablet
+
+DEFCONFIG=${TARGET}_${DEVICE}_defconfig
+DEFCONFIG_FILE=$RDIR/arch/$ARCH/configs/$DEFCONFIG
+
+[ -f "$DEFCONFIG_FILE" ] ||
+ABORT "Config $DEFCONFIG not found in $ARCH configs!"
+
+cd "$RDIR" || ABORT "Could not enter $RDIR!"
+
 echo "Cleaning build..."
 rm -rf build
 mkdir build
 make -s -i -C "$RDIR" O=build "$DEFCONFIG" menuconfig
 echo "Showing differences between old config and new config"
 echo "-----------------------------------------------------"
-command -v colordiff >/dev/null 2>&1 && {
+if command -v colordiff >/dev/null 2>&1; then
 	diff -Bwu --label "old config" "$DEFCONFIG_FILE" --label "new config" build/.config | colordiff
-} || {
+else
 	diff -Bwu --label "old config" "$DEFCONFIG_FILE" --label "new config" build/.config
 	echo "-----------------------------------------------------"
 	echo "Consider installing the colordiff package to make diffs easier to read"
-}
+fi
 echo "-----------------------------------------------------"
 echo -n "Are you satisfied with these changes? Y/N: "
-read option
+read -r option
 case $option in
 y|Y)
 	cp build/.config "$DEFCONFIG_FILE"
